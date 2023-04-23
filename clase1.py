e3b5312143fcbecda7bfeaa9b5cf3ec5d6997898
Esteban <cheo28sc@hotmail.com>
2023-04-23 14:49:52 -0600

    CORAZON

---
+++ clase1.py
@@ -0,0 +1,87 @@
+# last_name = " SEGURA"
+# name = "ESTEBAN"
+# print(name+""+last_name)
+
+# name = "ESTEBAN"
+# id = 112180982
+# money =  0.02
+# turn_on = True
+
+print ("Load Data")
+status = False
+print(status)
+
+is_day = False
+lights_on = not is_day
+
+print ("Daytime?")
+print(is_day)
+
+print ("lingts on")
+print (lights_on)
+
+#codigo de ventas y stock
+# stock = 600
+# jeans_sold = 500
+# target = 500
+# target_hit = jeans_sold == target
+# print ("hit jean sale target")
+# print (target_hit)
+# current_stock  = stock - jeans_sold
+# in_stock = current_stock != 0
+# print ("jeans in stock")
+# print (in_stock)
+
+#comparacion de numeros y ejemplos
+# result = 1 < 15
+# print (result)
+# min = 5
+# max = 10
+# result = min <= max
+# print (result)
+
+# battery_level = 20
+# low = battery_level <= 14
+# print ("low battery : ")
+# print (low)
+
+# print ("online" ==  "online")
+# print ("online" != "offline")
+
+#medidor de frecuencia cardiaca
+# hear_rate = 150
+
+# too_low = hear_rate < 40
+# too_high = hear_rate > 140
+# print ("heart rate low:")
+# print (too_low)
+
+# print ("heart rate higth: ")
+# print (too_high)
+
+#cadema de formato
+# new_masages = 5 
+# print ("f{new_massages} new message")
+# new = 5
+# read = 2
+# print ("f{new-read} unread")
+
+# CONDICIONALES
+# is_charged = False
+# if is_charged:
+#    print("Charged") 
+#    print ("Low Battery")
+    
+#   answer = "Matisse"
+#   if answer == "Picasso":
+#    print (answer = "is correcto") 
+
+# hour = 14
+# if  hour < 12:
+#     print("good morning")
+#     elif hour < 17
+#     print("good afternot")
+# else:
+#     print ("good night")
+    
+    
